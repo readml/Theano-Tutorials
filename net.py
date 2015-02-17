d1d7d8d9236b9c34c071c3dbca96204b3619afc4
themythicaldrago <ryan.louie@students.olin.edu>
2015-02-17 14:26:37 -0500

    added params at top of file and top of figure for easier experimentation

--- net.py
+++ net.py
@@ -4,15 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N_EXAMPLES = 50
+EPOCHS = 2 * 10**2
+LEARNING_RATE = 0.1
+WEIGHT_INIT_STD = 0.5
+X_DOMAIN = [-5.0, 5.0]
+
+
+trX, trY = sinusoid()
+
+print trX.shape
+print trY.shape
+
+def x2():
+    return create(func=np.square)
+
+def absolute():
+    return create(func=np.abs)
+
+def sinusoid():
+    return create(func=np.sin)
 
 def floatX(X):
     return np.asarray(X, dtype=theano.config.floatX)
 
-def init_weights(shape):
-    return theano.shared(floatX(np.random.randn(*shape) * 0.01))
+def init_weights(shape, std=WEIGHT_INIT_STD):
+    return theano.shared(floatX(np.random.randn(*shape) * std))
 
 # I don't currently understand if this is doing batches, or single training points
-def sgd(cost, params, lr=0.03):
+def sgd(cost, params, lr=LEARNING_RATE):
     grads = T.grad(cost=cost, wrt=params)
     updates = []
     for p, g in zip(params, grads):
@@ -39,29 +58,15 @@ def hidden_out(X, w_h, w_o):
     # transpose to do element wise multiply
     return h * w_o.T
 
-def create(func, xlow=-5.0, xhigh=5.0, bias=True):
+def create(func, xlow=X_DOMAIN[0], xhigh=X_DOMAIN[1], bias=True, std=0.5):
     trX = np.linspace(xlow, xhigh, N_EXAMPLES + 1)
 
-    trY = func(trX) + np.random.randn()*0.1
+    trY = func(trX) + np.random.randn()*std
     
     if bias:
         trX = np.column_stack((np.ones(trX.shape[0]), trX))
 
     return trX, trY
-
-def x2():
-    return create(func=np.square)
-
-def absolute():
-    return create(func=np.abs)
-
-def sinusoid():
-    return create(func=np.sin)
-
-trX, trY = sinusoid()
-
-print trX.shape
-print trY.shape
 
 # will train one row of X
 X = T.fvector(name='X') 
@@ -76,8 +81,8 @@ hidden nodes o o o
               \/\/
 input nodes   o  o 
 """
-w_h = init_weights((2, 3))
-w_o = init_weights((3, 1))
+w_h = init_weights((2, 4))
+w_o = init_weights((4, 1))
 
 out= model(X, w_h, w_o)
 hidden_out = hidden_out(X, w_h, w_o)
@@ -90,7 +95,7 @@ train = theano.function(inputs=[X, y], outputs=[cost, w_h], updates=updates, all
 predict = theano.function(inputs=[X], outputs=out, allow_input_downcast=True)
 predict_hidden_out = theano.function(inputs=[X], outputs=hidden_out, allow_input_downcast=True)
 
-for i in range(5 * 10**3):
+for i in range(EPOCHS):
     for X, y in zip(trX, trY):
         # X is a row in trX. X is shape (2,)
         # y is an element of trY. y is shape () 
@@ -111,6 +116,8 @@ for node_j in range(hidden_outputs.shape[1]):
     plt.plot(trX[:,1], hidden_outputs[:,node_j], label='hidden unit %d' % node_j)
 
 plt.legend()
+plt.figtext(.5,.95,'learning rate | weight init std | num examples | epochs', fontsize=14, ha='center')
+plt.figtext(.5,.91,'{} | {} | {}'.format(LEARNING_RATE, WEIGHT_INIT_STD, N_EXAMPLES, EPOCHS),fontsize=14,ha='center')
 plt.show()
 
 
